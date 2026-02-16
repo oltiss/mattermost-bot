75c72f017d18361ca05634896a97e2a38e88454f
oltiss <czstolec@outlook.com>
2026-02-16 21:07:14 +0000

    pppoe slash command

--- slash_commands.py
+++ slash_commands.py
@@ -18,16 +18,24 @@ def slash_coms(token, id):
 
 
     if token == ID_TOKEN:
-        prompt = f"""Użytkownik podał ci właśnie klienta o id:{id} z tabeli clients,
+        prompt = f"""Użytkownik podał ci właśnie klienta o client_id = {id} z tabeli clients,
                      która znajduje się w schemacie {DB_SCHEMA}. Masz na celu znalezienie rekordu
-                     z tym id, a następnie z kolumny client pobierz dane. Dane te są zapisane w JSON object.
+                     z tym id, a następnie z kolumny 'client' pobierz dane. Dane te są zapisane w JSON object.
                      Jako odpowiedź wypisz tylko te dane z odpowiednimi opisami.
-                     Jeśli coś jest równe null, false, 0 lub empty string i tak masz to wypisać."""
+                     Jeśli coś jest równe null, false, 0 lub empty string i tak masz to wypisać.
+                     Jeśli nie możesz znaleźć rekordu z tym id, poinformuj o tym użytkownika,
+                     a nie wymyślaj danych."""
 
 
 
     if token == PPPOE_TOKEN:
-        prompt = f"""  """
+        prompt = f"""Użytkownik podał ci właśnie klienta o ip_id = {id} z tabeli hardware_ips,
+                     która znajduje się w schemacie {DB_SCHEMA}. Masz na celu znalezienie rekordu
+                     z tym id, a następnie z kolumny 'ip' pobierz dane. Dane te są zapisane w JSON object.
+                     Jako odpowiedź wypisz tylko te dane z odpowiednimi opisami.
+                     Jeśli coś jest równe null, false, 0 lub empty string i tak masz to wypisać.
+                     Jeśli nie możesz znaleźć rekordu z tym id, poinformuj o tym użytkownika,
+                     a nie wymyślaj danych."""
 
 
     return prompt
